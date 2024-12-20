c4a6f64eb30eaea73fcb3969b4657d6ddc28ac03
Jiri Sevcik <jiri.sevcik@primehammer.com>
2024-12-20 16:52:46 +0100

    Base structure for the event result

--- event_models/trigger/model/result.py
+++ event_models/trigger/model/result.py
@@ -3,32 +3,19 @@ from typing import Any
 
 from pydantic import UUID4, BaseModel, model_validator
 
-from event_models.event.event import MessageHeader
 from event_models.trigger.enum import ScrapType
 
 
 class EventResultHeader(BaseModel):
-    message_id: UUID4 | str
-    event_id: str
+    message_id: UUID4
     scrap_type: ScrapType
-    data_process_success: bool
+    process_result: bool
     finished: datetime.datetime
     error_reason: str | None = None
 
     @model_validator(mode="before")
     def check_failure(cls: Any, values: Any) -> Any:
-        if values["data_process_success"] is False and values.get("error_reason") is None:
+        if values["process_result"] is False and values.get("error_reason") is None:
             raise ValueError("error must be provided if the event result has a failed status.")
 
         return values
-
-    @staticmethod
-    def message_header_to_result_dict(message_header: MessageHeader, error_reason: str | None = None) -> dict[str, Any]:
-        return {
-            "message_id": message_header.event_message_id,
-            "event_id": message_header.event_id,
-            "scrap_type": ScrapType(message_header.event_source),
-            "data_process_success": error_reason is None,
-            "finished": datetime.datetime.now(),
-            "error_reason": error_reason,
-        }
