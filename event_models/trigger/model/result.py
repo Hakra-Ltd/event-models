599c707bdd0914fd9ebc68c842f3dd5d639d739c
Jiri Sevcik <jiri.sevcik@primehammer.com>
2025-01-07 15:10:45 +0100

    Create result from the message header

--- event_models/trigger/model/result.py
+++ event_models/trigger/model/result.py
@@ -3,6 +3,7 @@ from typing import Any
 
 from pydantic import UUID4, BaseModel, model_validator
 
+from event_models.event.event import MessageHeader
 from event_models.trigger.enum import ScrapType
 
 
@@ -19,3 +20,13 @@ class EventResultHeader(BaseModel):
             raise ValueError("error must be provided if the event result has a failed status.")
 
         return values
+
+    @classmethod
+    def from_message_header(cls, message_header: MessageHeader, error_reason: str | None = None) -> "EventResultHeader":
+        return cls(
+            message_id=message_header.event_message_id,
+            scrap_type=ScrapType(message_header.event_source),
+            data_process_success=error_reason is None,
+            finished=datetime.datetime.now(),
+            error_reason=error_reason,
+        )
