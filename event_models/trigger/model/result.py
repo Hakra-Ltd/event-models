19e4d36ff5177b8afb36e33a497435f6275ce074
Jiri Sevcik <jiri.sevcik@primehammer.com>
2025-01-07 14:55:46 +0100

    Add success flag

--- event_models/trigger/model/result.py
+++ event_models/trigger/model/result.py
@@ -9,13 +9,13 @@ from event_models.trigger.enum import ScrapType
 class EventResultHeader(BaseModel):
     message_id: UUID4
     scrap_type: ScrapType
-    process_result: bool
+    data_process_success: bool
     finished: datetime.datetime
     error_reason: str | None = None
 
     @model_validator(mode="before")
     def check_failure(cls: Any, values: Any) -> Any:
-        if values["process_result"] is False and values.get("error_reason") is None:
+        if values["data_process_success"] is False and values.get("error_reason") is None:
             raise ValueError("error must be provided if the event result has a failed status.")
 
         return values
