82c8add267cd59f9f692df4d3b13fa1c204c50f9
Jiri Sevcik <jiri.sevcik@primehammer.com>
2025-01-09 08:10:44 +0100

    Add event id

--- event_models/trigger/model/result.py
+++ event_models/trigger/model/result.py
@@ -9,6 +9,7 @@ from event_models.trigger.enum import ScrapType
 
 class EventResultHeader(BaseModel):
     message_id: UUID4
+    event_id: str
     scrap_type: ScrapType
     data_process_success: bool
     finished: datetime.datetime
@@ -25,6 +26,7 @@ class EventResultHeader(BaseModel):
     def from_message_header(cls, message_header: MessageHeader, error_reason: str | None = None) -> "EventResultHeader":
         return cls(
             message_id=message_header.event_message_id,
+            event_id=message_header.event_id,
             scrap_type=ScrapType(message_header.event_source),
             data_process_success=error_reason is None,
             finished=datetime.datetime.now(),
