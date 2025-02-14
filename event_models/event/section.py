77133de3796bd131719467ce39d4c7d480e31789
Jiri Sevcik <jiri.sevcik@primehammer.com>
2025-02-14 13:06:25 +0100

    Add vivid section header

--- event_models/event/section.py
+++ event_models/event/section.py
@@ -1,7 +1,12 @@
+import datetime
 from typing import Optional
 
 from pydantic import BaseModel, NonNegativeInt
 
+
+class VividseatsSectionHeader(BaseModel):
+    event_id: str
+    event_timestamp: datetime.datetime
 
 class VividseatsSectionInfo(BaseModel):
     section: str
