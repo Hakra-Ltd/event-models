ddea5093c1d0e464a9c8df34d8a0e3b349facb39
jiri <jiri@mirrorlabs.co>
2025-12-11 09:11:23 +0100

    Ignore type check for PriceMarkup definition

--- event_models/action/action.py
+++ event_models/action/action.py
@@ -2,13 +2,13 @@ import datetime
 import enum
 from collections import defaultdict
 from decimal import Decimal
-from typing import Any, Optional, DefaultDict, Annotated
+from typing import Annotated, Any, Optional, DefaultDict
 
 from pydantic import BaseModel, Field, model_validator
 
 from event_models.exchange.exchange import EventExchange
 
-type PriceMarkup = DefaultDict[
+type PriceMarkup = DefaultDict[  # type: ignore[valid-type]
     EventExchange,
     Annotated[Decimal, Field(default_factory=Decimal)],
 ]
